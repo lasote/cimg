1622957f620af596d87ec9e217a35aa363cc9e6d
ronounours <David.Tschumperle@ensicaen.fr>
2013-09-19 14:25:59 +0200

    -

--- CImg.h
+++ CImg.h
@@ -4263,38 +4263,7 @@ namespace cimg_library_suffixed {
     }
 
     // Implement a tic/toc mechanism to display elapsed time of algorithms.
-    inline unsigned long tictoc(const bool is_tic) {
-      cimg::mutex(2);
-      static unsigned long t0 = 0;
-      const unsigned long t = cimg::time();
-      if (is_tic) { cimg::mutex(2,0); return (t0 = t); }
-      const unsigned long dt = t>=t0?(t - t0):cimg::type<unsigned long>::max();
-      const unsigned int
-        edays = (unsigned int)(dt/86400000.0),
-        ehours = (unsigned int)((dt - edays*86400000.0)/3600000.0),
-        emin = (unsigned int)((dt - edays*86400000.0 - ehours*3600000.0)/60000.0),
-        esec = (unsigned int)((dt - edays*86400000.0 - ehours*3600000.0 - emin*60000.0)/1000.0),
-        ems = (unsigned int)(dt - edays*86400000.0 - ehours*3600000.0 - emin*60000.0 - esec*1000.0);
-      if (!edays && !ehours && !emin && !esec)
-        std::fprintf(cimg::output(),"%s[CImg] Elapsed time: %u ms%s\n",cimg::t_red,ems,cimg::t_normal);
-      else {
-        if (!edays && !ehours && !emin)
-          std::fprintf(cimg::output(),"%s[CImg] Elapsed time: %u sec %u ms%s\n",cimg::t_red,esec,ems,cimg::t_normal);
-        else {
-          if (!edays && !ehours)
-            std::fprintf(cimg::output(),"%s[CImg] Elapsed time: %u min %u sec %u ms%s\n",cimg::t_red,emin,esec,ems,cimg::t_normal);
-          else{
-            if (!edays)
-              std::fprintf(cimg::output(),"%s[CImg] Elapsed time: %u hours %u min %u sec %u ms%s\n",cimg::t_red,ehours,emin,esec,ems,cimg::t_normal);
-            else{
-              std::fprintf(cimg::output(),"%s[CImg] Elapsed time: %u days %u hours %u min %u sec %u ms%s\n",cimg::t_red,edays,ehours,emin,esec,ems,cimg::t_normal);
-            }
-          }
-        }
-      }
-      cimg::mutex(2,0);
-      return t;
-    }
+    inline unsigned long tictoc(const bool is_tic);
 
     //! Start tic/toc timer for time measurement between code instructions.
     /**
@@ -46119,6 +46088,52 @@ namespace cimg_library_suffixed {
   */
 
 namespace cimg {
+
+    // Implement a tic/toc mechanism to display elapsed time of algorithms.
+    inline unsigned long tictoc(const bool is_tic) {
+      cimg::mutex(2);
+      static CImg<unsigned long> times(64);
+      static unsigned int pos = 0;
+      const unsigned long t1 = cimg::time();
+      if (is_tic) { // Tic.
+        times[pos++] = t1;
+        if (pos>=times.width())
+          throw CImgArgumentException("cimg::tic(): Too much calls to 'cimg::tic()' without calls to 'cimg::toc()'.");
+        cimg::mutex(2,0);
+        return t1;
+      }
+      // Toc.
+      if (!pos)
+        throw CImgArgumentException("cimg::toc(): No previous call to 'cimg::tic()' has been made.");
+      const unsigned long
+        t0 = times[--pos],
+        dt = t1>=t0?(t1-t0):cimg::type<unsigned long>::max();
+      const unsigned int
+        edays = (unsigned int)(dt/86400000.0),
+        ehours = (unsigned int)((dt - edays*86400000.0)/3600000.0),
+        emin = (unsigned int)((dt - edays*86400000.0 - ehours*3600000.0)/60000.0),
+        esec = (unsigned int)((dt - edays*86400000.0 - ehours*3600000.0 - emin*60000.0)/1000.0),
+        ems = (unsigned int)(dt - edays*86400000.0 - ehours*3600000.0 - emin*60000.0 - esec*1000.0);
+      if (!edays && !ehours && !emin && !esec)
+        std::fprintf(cimg::output(),"%s[CImg]%*sElapsed time: %u ms%s\n",cimg::t_red,1+2*pos,"",ems,cimg::t_normal);
+      else {
+        if (!edays && !ehours && !emin)
+          std::fprintf(cimg::output(),"%s[CImg]%*sElapsed time: %u sec %u ms%s\n",cimg::t_red,1+2*pos,"",esec,ems,cimg::t_normal);
+        else {
+          if (!edays && !ehours)
+            std::fprintf(cimg::output(),"%s[CImg]%*Elapsed time: %u min %u sec %u ms%s\n",cimg::t_red,1+2*pos,"",emin,esec,ems,cimg::t_normal);
+          else{
+            if (!edays)
+              std::fprintf(cimg::output(),"%s[CImg]%*Elapsed time: %u hours %u min %u sec %u ms%s\n",cimg::t_red,1+2*pos,ehours,emin,esec,ems,cimg::t_normal);
+            else{
+              std::fprintf(cimg::output(),"%s[CImg]%*Elapsed time: %u days %u hours %u min %u sec %u ms%s\n",cimg::t_red,1+2*pos,edays,ehours,emin,esec,ems,cimg::t_normal);
+            }
+          }
+        }
+      }
+      cimg::mutex(2,0);
+      return dt;
+    }
 
   //! Display a simple dialog box, and wait for the user's response.
   /**
