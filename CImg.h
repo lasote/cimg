c4f920c05333fd1098538e753e255eb8aef650dd
ronounours <David.Tschumperle@ensicaen.fr>
2014-06-07 14:38:30 +0200

    -

--- CImg.h
+++ CImg.h
@@ -15729,12 +15729,24 @@ namespace cimg_library_suffixed {
       CImg<doubleT> res(1,xyzc.size()/4);
       if (!expression) return res.fill(0);
       _cimg_math_parser mp(*this,expression,"eval");
+#ifdef cimg_use_openmp
+#pragma omp parallel if (xyzc.size()>512)
+      {
+        _cimg_math_parser _mp = omp_get_thread_num()?mp:_cimg_math_parser(), &lmp = omp_get_thread_num()?_mp:mp;
+#pragma omp for
+        for (unsigned int i = 0; i<res._height; ++i) {
+          const unsigned int i4 = 4*i;
+          const double x = (double)xyzc[i4], y = (double)xyzc[i4+1], z = (double)xyzc[i4+2], c = (double)xyzc[i4+3];
+          res[i] = lmp(x,y,z,c);
+        }
+      }
+#else
       const t *ps = xyzc._data;
-      double x, y, z, c;
       cimg_for(res,pd,double) {
-        x = (double)*(ps++); y = (double)*(ps++); z = (double)*(ps++); c = (double)*(ps++);
+        const double x = (double)*(ps++); y = (double)*(ps++); z = (double)*(ps++); c = (double)*(ps++);
         *pd = mp(x,y,z,c);
       }
+#endif
       return res;
     }
 
